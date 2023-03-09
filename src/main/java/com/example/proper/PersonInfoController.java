89d4a23be650f345c8ff659266b91d82f2e6b009
Shruti Patel <sbpatel9997@gmail.com>
2023-03-09 15:17:04 +0530

    change in swagger

--- src/main/java/com/example/proper/PersonInfoController.java
+++ src/main/java/com/example/proper/PersonInfoController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("user")
+@CrossOrigin("*")
 public class PersonInfoController {
 
     @Autowired
     PersonInfoService personInfoService;
 
-    @PostMapping("/add-person-info")
+    @PostMapping("/add")
     public ResponseEntity<PersonInfo> createPersonInfo(@RequestBody PersonInfo personInfo) {
         PersonInfo response = personInfoService.createPersonInfo(personInfo);
 
@@ -21,7 +23,7 @@ public class PersonInfoController {
 
     }
 
-    @GetMapping("/get-all-person-info")
+    @GetMapping("/get-all")
     public ResponseEntity<List<PersonInfo>> getPersonInfo() {
         List<PersonInfo> response = personInfoService.getPersonInfo();
 
@@ -29,7 +31,7 @@ public class PersonInfoController {
 
     }
 
-    @GetMapping("/get-person-info-by-id/{id}")
+    @GetMapping("/get-by-id/{id}")
     public ResponseEntity<PersonInfo> getPersonInfoById(@PathVariable String id) {
         PersonInfo response = personInfoService.getPersonInfoById(id);
 
@@ -39,7 +41,7 @@ public class PersonInfoController {
 
 
 
-    @DeleteMapping("/delete-person-info-by-id/{id}")
+    @DeleteMapping("/delete-by-id/{id}")
     public ResponseEntity deletePersonInfo(@PathVariable String id) {
         personInfoService.deletePersonInfo(id);
 
--- src/main/java/com/example/proper/ProperApplication.java
+++ src/main/java/com/example/proper/ProperApplication.java
@@ -2,10 +2,8 @@ package com.example.proper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class ProperApplication {
 
 	public static void main(String[] args) {
